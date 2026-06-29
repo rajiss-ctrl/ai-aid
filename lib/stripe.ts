357557506dcf5cbd44cddea3c70553462d601afd
rajiss-ctrl <omosanjos77@gmail.com>
2026-06-28 17:05:01 -0700

    Correct version issue in lib/stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-05-27.dahlia",
+  apiVersion: "2026-06-24.dahlia",
   typescript: true,
 });
 
